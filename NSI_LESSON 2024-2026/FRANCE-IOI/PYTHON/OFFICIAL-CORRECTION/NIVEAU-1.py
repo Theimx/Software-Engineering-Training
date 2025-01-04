48630f88ff84a7730b4c423c72edf84f1abadf10
FaimaliG <85684007+Theimx@users.noreply.github.com>
2025-01-05 00:59:33 +0100

    Add files via upload

--- NSI_LESSON 2024-2026/FRANCE-IOI/PYTHON/OFFICIAL-CORRECTION/NIVEAU-1.py
+++ NSI_LESSON 2024-2026/FRANCE-IOI/PYTHON/OFFICIAL-CORRECTION/NIVEAU-1.py
@@ -30,3 +30,42 @@ def exo3():
             gauche()  
         deposer()
         anneau = anneau + 1
+
+def exo4():
+    nbCubes = 0
+    largeurArête = 1
+    for loop in range(9):
+        nbCubes = nbCubes + largeurArête * largeurArête * largeurArête
+        largeurArête = largeurArête + 2
+    print(nbCubes)
+
+def exo5():
+    positionDépart = int(input())
+    largeurEmplacement = int(input())
+    nbVendeurs = int(input())
+
+    position = positionDépart
+    for iVendeur in range(nbVendeurs + 1):
+        print(position)
+        position = position + largeurEmplacement
+
+def exo6():
+    largeurBas = int(input())
+    largeurHaut = int(input())
+    
+    volume = 0
+    largeur = largeurHaut
+    for loop in range(largeurBas - largeurHaut + 1):
+        volume = volume + largeur * largeur
+        largeur = largeur + 1
+        
+    print(volume)
+
+def exo7():
+    nbKarvas = int(input())
+    for loop in range(nbKarvas):
+        poids = int(input())
+        âge = int(input())
+        longueurCornes = int(input())
+        hauteurAuGarrot = int(input())
+        print(longueurCornes * hauteurAuGarrot + poids)
