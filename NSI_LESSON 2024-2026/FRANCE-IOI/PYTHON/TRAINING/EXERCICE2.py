b0a3efaa95dbb9fecfade184a93ace3b8f87795b
FaimaliG <85684007+Theimx@users.noreply.github.com>
2025-01-12 19:29:30 +0100

    Add files via upload

--- NSI_LESSON 2024-2026/FRANCE-IOI/PYTHON/TRAINING/EXERCICE2.py
+++ NSI_LESSON 2024-2026/FRANCE-IOI/PYTHON/TRAINING/EXERCICE2.py
@@ -1,4 +1,4 @@
-
+import math
 
 #Liste des exercices obligatoires du Niveau 2 sur le site France-IOI 
 #https://www.france-ioi.org/algo/chapters.php
@@ -11,3 +11,12 @@ price_ttc = float(input())
 price_ht = price_ttc / (1 + actual_tax)
 new_price_ttc = price_ht * (1 + new_tax)
 print(round(new_price_ttc, 2))
+
+#exo 2
+
+money = float(input())
+book = float(input())
+print(round((money//book)))
+
+#or 
+print(round(float(input())//float(input())))
