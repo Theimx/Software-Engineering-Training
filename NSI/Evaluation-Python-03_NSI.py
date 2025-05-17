e385556796c202a873bc5bb17352017ceb1fa425
Theimx <85684007+Theimx@users.noreply.github.com>
2025-05-17 22:54:57 +0200

    Add files via upload

--- NSI/Evaluation-Python-03_NSI.py
+++ NSI/Evaluation-Python-03_NSI.py
@@ -3,3 +3,27 @@
 # Note : 20/20
 
 #Exercice 1 : 
+
+def nb_mot(_chaine): 
+    if _chaine :
+        total = 0 
+        for i in _chaine:
+            if i == " " or i == "'":
+                total += 1 
+        return(total)
+    return(0)
+
+#print(nb_mot(" J'aime la NSI")) # >>>  4
+
+#Exercice 2 : 
+def Minmax(tab):
+    mini = tab[0]
+    maxi = tab[0]
+    for i in tab:
+        if i >= maxi :
+            maxi = i
+        if i <= mini :
+            mini = i
+    return(mini,maxi,(maxi-mini))
+
+#print(Minmax([1,4,6,8,19,4,2,0]))  # >>> (0, 19, 19)
