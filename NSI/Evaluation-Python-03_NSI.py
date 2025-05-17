47de426ecafb2b0b66058d0aa5246adfd78c9d7d
Theimx <85684007+Theimx@users.noreply.github.com>
2025-05-17 23:54:50 +0200

    Add files via upload

--- NSI/Evaluation-Python-03_NSI.py
+++ NSI/Evaluation-Python-03_NSI.py
@@ -2,8 +2,10 @@
 # Classe de 1 ere 
 # Note : 20/20
 
-#Exercice 1 : 
+# Exercice 1 : 
 
+# Ecrire une fonction nb_mot qui détermine le nombre de mots dans une chaine de charactère,
+# renvoie 0 si le str est nul.
 def nb_mot(_chaine): 
     if _chaine :
         total = 0 
@@ -16,6 +18,10 @@ def nb_mot(_chaine):
 #print(nb_mot(" J'aime la NSI")) # >>>  4
 
 #Exercice 2 : 
+
+# Ecrire une fonction Minmax qui prend une liste d'entier en paramètre et qui 
+# détermine le plus grand et le plus petit élément d'une liste. La fonction 
+# calcul aussi l'écart type entre le plus grand et le plus petit.
 def Minmax(tab):
     mini = tab[0]
     maxi = tab[0]
