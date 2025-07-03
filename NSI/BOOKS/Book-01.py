df6812b47300edbeb22c548fcf899d338fd5a90c
Theimx <85684007+Theimx@users.noreply.github.com>
2025-07-03 14:42:50 +0200

    Add files via upload

---
+++ NSI/BOOKS/Book-01.py
@@ -0,0 +1,5 @@
+# Exercices tiré du livre : Apprendre à programmer avec Python 3, 3 ème édition
+# écrit par Gérard Swinnen 
+# ISBN : 978-2-212-13434-6
+# nombre d'éxercices : 60
+# validé : 01/60  
