6030d78e9904cd849540ddf82d35c92d04181fc0
Theimx <85684007+Theimx@users.noreply.github.com>
2025-09-07 14:48:13 +0200

    Delete NSI directory

--- NSI/BOOKS/Book-01.py
+++
@@ -1,5 +0,0 @@
-# Liste des exercices tiré du livre : Apprendre à programmer avec Python 3, 3 ème édition
-# Ecrit par Gérard Swinnen 
-# ISBN : 978-2-212-13434-6
-# Nombre d'éxercices : 60
-# Validé : 01/60  
--- NSI/BOOKS/Book-02.py
+++
@@ -1,5 +0,0 @@
-# Liste des exercices tiré du livre : Algorithmique, Techniques fondamentales de programmation.
-# Ecrit par Frank EBEL, Sébastien ROHAUT
-# ISBN : 978-2-409-01226
-# Nombre d'éxercices : 60
-# Validé : 01/60  
