9fd9d634a0adeedfd1f6c277dce89cf46633f3aa
Theimx <85684007+Theimx@users.noreply.github.com>
2025-09-16 09:52:32 +0200

    Add files via upload

--- NSI/BNS-2025.py
+++ NSI/BNS-2025.py
@@ -176,6 +176,42 @@ def binaire42(a):
 # Epreuves Pratiques -BNS 2025- sujet n°43 
 
 # Epreuves Pratiques -BNS 2025- sujet n°44
+def moyenne44(tab):
+    note,coef = 0,0
+    for i in range(len(tab)):
+        note += tab[i][0] * tab[i][1]
+        coef += tab[i][1]
+    return(note/coef)
+
+def affiche(dessin):
+    ''' affichage d'une grille : les 1 sont représentés par 
+        un "*" , les 0 par un espace " ". '''
+    for ligne in dessin:
+        affichage = ''
+        for col in ligne:
+            if col == 1:
+                affichage = affichage + "*"
+            else :
+                affichage = affichage + " "
+        print(affichage)
+def liste_zoom(liste_depart,k):
+    ''' renvoie une liste contenant k fois chaque élément de 
+        liste_depart '''
+    liste_zoomee = ...
+    for elt in ... : 
+        for i in range(k):
+            ...
+    return liste_zoomee
+
+def dessin_zoom(grille,k):
+    '''renvoie une grille ou les lignes sont zoomées k fois 
+       ET répétées k fois'''
+    grille_zoomee = []
+    for ligne in grille:
+        ligne_zoomee = ...
+        for i in range(k):
+            ... .append(...)
+    return grille_zoomee
 
 # Epreuves Pratiques -BNS 2025- sujet n°45
 
