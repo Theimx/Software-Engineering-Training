cc3ec1087295d5cfafc601dea0e63daff757eb4f
Theimx <85684007+Theimx@users.noreply.github.com>
2025-09-16 09:06:04 +0200

    Add files via upload

--- NSI/BNS-2025.py
+++ NSI/BNS-2025.py
@@ -54,6 +54,28 @@ def insert13(tab,a):
 # Epreuves Pratiques -BNS 2025- sujet n°17
 
 # Epreuves Pratiques -BNS 2025- sujet n°18
+def moyenne18(tab):
+    somme = 0
+    for i in tab:
+        somme += i
+    return float(somme/len(tab))
+
+def dichotomie18(tab,x):
+    """applique une recherche dichotomique pour déterminer
+       si x est dans le tableau trié tab.
+       La fonction renvoie True si tab contient x et False sinon"""
+    
+    debut = 0
+    fin = len(tab) -1
+    while debut <= fin:
+        m = (debut + fin ) // 2
+        if x == tab[m]:
+            return True
+        if x > tab[m]:
+            debut = m
+        else :
+            fin = m
+    return False
 
 # Epreuves Pratiques -BNS 2025- sujet n°19
 
