ca61b3ac713d5b772b7e20be8fc88b94dc95b3ad
Theimx <85684007+Theimx@users.noreply.github.com>
2025-09-30 08:51:45 +0200

    Add files via upload

--- NSI/BNS-2025.py
+++ NSI/BNS-2025.py
@@ -164,6 +164,29 @@ def vainqueurs27(election):
 
 # Epreuves Pratiques -BNS 2025- sujet n°29
 
+def selection_enclos(tab,num_enclos):
+    res = []
+    for i in range(len(tab)):
+        if tab[i]['enclos'] == num_enclos:
+            res.append(tab[i])
+    return res
+
+def trouver_intrus(tab,g,d):
+    """Renvoie la valeur de l'intrus situé entre les indices g et d
+       dans le tableau tab ou : 
+       tab verifie les conditions de l'exercice,
+       g et d sont des multiples de 3."""
+    if g == d:
+        return ...
+    else : 
+        nombre_de_triplets = (d - g) // ... # 3
+        indice = g + 3 * (nombre_de_triplet) 
+        if ... :
+            return ...
+        else : 
+            return ...
+
+
 # Epreuves Pratiques -BNS 2025- sujet n°30
 
 # Epreuves Pratiques -BNS 2025- sujet n°31
